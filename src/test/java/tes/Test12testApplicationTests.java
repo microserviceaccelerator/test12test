c49a7244659f9222f6f0bcd1f24740b981382133
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-20 17:09:22 +0530

    Initial Commit

---
+++ src/test/java/tes/Test12testApplicationTests.java
@@ -0,0 +1,13 @@
+package tes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test12testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
